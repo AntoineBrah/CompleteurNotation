0a3881a118fd673de8b75c847ecc2ac0ec5a8b2e
Antoine <antoinebrah@gmail.com>
2020-02-23 19:41:49 +0100

    Interface graphique en cours de réalisation...

--- Interface/cppData.js
+++ Interface/cppData.js
@@ -1,5 +1,8 @@
 var chessboard = document.querySelectorAll('#chessboard div');
 
+var rightButton = document.querySelector('.right-button');
+var leftButton = document.querySelector('.left-button');
+
 /************************/
 /*   REQUETE CELLULES   */
 /************************/
@@ -10,27 +13,90 @@ requete.open('GET', "cellules.json", true);
 requete.responseType = 'text';
 requete.send();
 
+var cellulesInitiales = [];
+var cellulesNouvelles = [];
+
 
 requete.onload = function(){
     if(requete.readyState == 4 && requete.status == 200){
 
         var response = requete.response;
 
-        var myTab = JSON.parse(response);
+        var tmpTab = JSON.parse(response);
 
-        for(var i=0; i<chessboard.length; i++){
-            for(var j=0; j<myTab.length; j++){
-                
-                if(chessboard[i].getAttribute('id') === myTab[j].position){
-                    
-                    chessboard[i].style.backgroundImage = 'url(' + 'images/pieces/' + myTab[j].couleur + '/' + myTab[j].piece + '.svg)';
-                }
-            }
+        for(var i=0; i<32; i++){
+            cellulesInitiales.push(tmpTab[i]);
         }
 
+        for(var i=32; i<tmpTab.length; i++){
+            cellulesNouvelles.push(tmpTab[i]);
+        }
+
+        // On instancie le plateau
+        instanciationPlateau(cellulesInitiales);
 
 
+        var indexCoup = 0;
 
+        rightButton.addEventListener('click', function(){
+            console.log(cellulesNouvelles[cellulesNouvelles.length-1]);
+            console.log(indexCoup);
+
+            if(indexCoup>cellulesNouvelles.length-1){
+                indexCoup = cellulesNouvelles.length-1;
+            }
+            else{
+                putAPieceOnAPosition(cellulesNouvelles[indexCoup].position, cellulesNouvelles[indexCoup].piece, cellulesNouvelles[indexCoup].couleur, cellulesNouvelles[indexCoup].adr, cellulesNouvelles[indexCoup].csp);
+                indexCoup++;
+            }
+        });
+
+        leftButton.addEventListener('click', function(){
+            if(indexCoup<0){
+                indexCoup = 0;
+            }
+            else{
+                pullAPieceFromAPosition(cellulesNouvelles[indexCoup].position);
+                indexCoup--;
+            }
+        });
+
+
+    }
+}
+
+
+function instanciationPlateau(cellulesInitiales){
+    for(var i=0; i<chessboard.length; i++){
+        for(var j=0; j<cellulesInitiales.length; j++){
+            if(chessboard[i].getAttribute('id') === cellulesInitiales[j].position){
+                chessboard[i].setAttribute('class', cellulesInitiales[j].adr + ',' + cellulesInitiales[j].csp);
+                chessboard[i].style.backgroundImage = 'url(' + 'images/pieces/' + cellulesInitiales[j].couleur + '/' + cellulesInitiales[j].piece + '.svg)';
+            }
+        }
+    }
+}
+
+function putAPieceOnAPosition(position, piece, couleur, adr, csp){
+    for(var i=0; i<chessboard.length; i++){
+        if(chessboard[i].getAttribute('id') === position){
+            for(var j=0; j<chessboard.length; j++){
+                if(chessboard[j].getAttribute('class').split(',')[1] === adr){
+                    chessboard[j].setAttribute('class', '0,0');
+                    chessboard[j].style.backgroundImage = '';
+                }
+            }
+            chessboard[i].setAttribute('class', adr + ',' + csp);
+            chessboard[i].style.backgroundImage = 'url(' + 'images/pieces/' + couleur + '/' + piece + '.svg)';
+        }
+    }
+}
+
+function pullAPieceFromAPosition(position){
+    for(var i=0; i<chessboard.length; i++){
+        if(chessboard[i].getAttribute('id') === position){
+            chessboard[i].style.backgroundImage = '';
+        }
     }
 }
 
--- Main.cc
+++ Main.cc
@@ -26,53 +26,11 @@ int main(int argc, char* argv[]){
     else{
         
         lectureFichier File(argv[1]); // On ouvre le fichier txt en lecture
-
         
         if(File.getEstCorrectementOuvert()){ // Si le fichier est correctement ouvert alors, on le traite
 
             traitementCoups(&File);
         }
-        
-
-
-        /*
-        Piece *p1 = new Cavalier(Blanc, type::Cavalier, "g1");
-
-        Cellule *c1 = new Cellule(p1);
-        
-        p1->setPostion("g3");
-        updateListeCoupsPossiblesAll();
-
-        Cellule *c2 = new Cellule(p1);
-
-        c1->setCSP(c2); 
-
-        p1->setPostion("g5");
-        updateListeCoupsPossiblesAll();
-
-
-        Cellule *c3 = new Cellule(p1);
-
-        c2->setCSP(c3);
-    
-        cout << "Adresse c1 : " << c1 << "\nAdresse c2 : " << c2 << "\nAdresse c3 : " << c3 << "\nAdresse de CSP : " << getDernierCSP(c1) << endl;
-
-        //Cellule *c = getDernierCSP(c1);
-
-        //cout << "c pointe vers : " << c << endl;
-        */
-        
-        /*
-        vector<Cellule*> *initialisationEchiquier = initialiserCellules();
-
-        for(Cellule* cell : (*initialisationEchiquier)){
-            delete cell;
-        }
-
-        initialisationEchiquier->clear();
-        */
-
-       
     }
 
     correctionSyntaxe();
